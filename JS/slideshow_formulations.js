90f203bbda916ac1ba8a3d59d9d8d20fff43631d
Yash Shah <shahyash452@gmail.com>
2022-05-29 16:38:41 +0530

    This is tablet responsive

---
+++ JS/slideshow_formulations.js
@@ -0,0 +1,23 @@
+let sIndex = 1;
+showSlide(sIndex);
+
+function currentSlid(n) {
+    showSlide(sIndex = n);
+}
+
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("form-slider");
+    let dots = document.getElementsByClassName("dot1");
+    if (n > slides.length) { sIndex = 1 }
+    if (n < 1) { sIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[sIndex - 1].style.display = "block";
+    dots[sIndex - 1].className += " active";
+    // setTimeout(showSlide, 2000);
+}
